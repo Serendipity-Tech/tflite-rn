e05fc9035ab03f6fb820ac072241e1e891421464
Zeyu Xiong <scyzx2@gmail.com>
2021-09-22 16:11:17 +0800

    init commit

---
+++ ios/TfliteReactNative.h
@@ -0,0 +1,12 @@
+//
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+//#import "RCTBridgeModule.h"
+
+@interface TfliteReactNative : NSObject <RCTBridgeModule>
+
+@end
+  
